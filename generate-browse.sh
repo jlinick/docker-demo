15997d0edaa8561b43252f478ed1c6e62c7295fe
jlinick <jlinick@gmail.com>
2020-07-31 16:52:03 +0000

    correct error in JPEG compression flag for tiff generation

--- generate-browse.sh
+++ generate-browse.sh
@@ -13,10 +13,13 @@ for folder in ${allowed_polarizations[*]}; do
             filebase=$(echo $filename | sed 's/.tiff//g')
 	    projected_tiff=$folder/$filebase.projected.tiff
             # project to Geotiff, generate browse, and then kml
-            gdalwarp -t_srs EPSG:4326 -co COMPRESS=JPEG -co PHOTOMETRIC=MINISBLACK -ot Byte "$folder/$filename" "$projected_tiff"
-            gdal_translate -ot Byte -outsize 50% 50% "$projected_tiff" "$folder/$filebase.jpg"
+            gdalwarp -t_srs EPSG:4326 -co PHOTOMETRIC=MINISBLACK -ot Byte "$folder/$filename" "$projected_tiff"
+            gdal_translate -outsize 50% 50% -ot Byte "$projected_tiff" "$folder/$filebase.jpg"
             /usr/bin/gdal2tiles.py -k --srcnodata=0 --processes 16 -z 3-10 "$projected_tiff" "$folder/$filebase"
 	    rm -f "$projected_tiff"
+            # zip the files into a kmz
+	    cd $folder/$filebase && zip -r "../$filebase.kmz" * && cd ../..
+	    rm -rf $folder/$filebase && chmod -R 755 $folder
         done
     fi
 done
