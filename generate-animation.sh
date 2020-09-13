b10756549c0d738b2d2ccd7e1e0711a050539652
jlinick <jlinick@gmail.com>
2020-09-13 14:41:31 -0400

    fix some projection issues. still only projected

--- generate-animation.sh
+++ generate-animation.sh
@@ -33,36 +33,53 @@ gdal_time() {
 for folder in ${allowed_polarizations[*]}; do
     if [[ -d "${folder}" ]] ; then
         # if there are multiple browse files, we will generate an animated file
-	browse_count=$(find $folder -maxdepth 1 -name "*.scaled.tiff" -printf '%p\n' | sort -u | wc -l)
+	browse_count=$(find $folder -maxdepth 1 -name "*.merged.tiff" -printf '%p\n' | sort -u | wc -l)
 	if [ "${browse_count}" -gt "1" ] ; then
 
 	    echo "generating animation using ${browse_count} frames..."
             # generate merged shapefile
 	    shapefile_path=${folder}/boundary.shp
-            #gdaltindex -src_srs_format EPSG -src_srs_name src_srs -t_srs EPSG:${use_projection} ${folder}/extent.shp ${folder}/*.merged.tiff
-            ogrmerge.py -src_layer_field_name location -t_srs EPSG:3031 -o ${shapefile_path} ${kml_path} -single
-	    # cut all images to the shapefile
+            if [ ! -f "${shapefile_path}" ]; then
+                #gdaltindex -src_srs_format EPSG -src_srs_name src_srs -t_srs EPSG:${use_projection} ${folder}/extent.shp ${folder}/*.merged.tiff
+                ogrmerge.py -src_layer_field_name location -t_srs EPSG:3031 -o ${shapefile_path} ${kml_path} -single
+            fi
+
+            # cut all images to the shapefile
             iterator=1
-	    matching_files=$(find $folder -name "*.scaled.tiff" -printf '%p\n' | sort -u)
+	    matching_files=$(find $folder -name "*.merged.tiff" -printf '%p\n' | sort -u)
 	    for file in $matching_files
             do
 		filename="$(basename "${file}")"                              # full filename without the path
-	        filebase="$(echo ${filename} | sed 's/.'"scaled.tiff"'//g')"  # filename without extension
+	        filebase="$(echo ${filename} | sed 's/.'"merged.tiff"'//g')"  # filename without extension
 		counter=$(printf %03d $iterator)
-		warp="${folder}/${filebase}.${counter}.warped.vrt"
+		warp="${folder}/${filebase}.${counter}.cropped.vrt"
 		mpg="${folder}/${counter}.mpg.png"
 
                 # crop to the shapefile extent
-		gdalwarp -overwrite -cutline ${shapefile_path} -crop_to_cutline -dstalpha "${file}" "${warp}"
+
+		#gdalwarp -overwrite -cutline ${shapefile_path} -crop_to_cutline -srcalpha -dstalpha "${file}" "${warp}"
+		gdalwarp -overwrite -cutline ${shapefile_path} -crop_to_cutline -srcalpha -dstalpha "${file}" "${warp}"
 		# export as png
-                gdal_translate -outsize 4096 0 -of PNG -r nearest "${warp}" "${mpg}" # THIS IS EXTREMELY SLOW. NEED TO MAKE FASTER
+
+                #gdal_translate -outsize 4096 0 -of PNG -r nearest "${warp}" "${mpg}" # THIS IS EXTREMELY SLOW. NEED TO MAKE FASTER
+                gdal_translate -of PNG -r nearest -outsize 7424 6521 "${warp}" "${mpg}" # THIS IS EXTREMELY SLOW. NEED TO MAKE FASTER
+
+                # now we generate an overlay of prior images, to avoid flickering black pixels
+		if [ ${iterator} -eq 1 ]; then
+                    cp "${mpg}" "${folder}/base.png" # just copy the image
+	        else
+		    # composite background_image overlay_image result_image
+		    convert -composite "${folder}/base.png" "${mpg}" "${folder}/${counter}.finished.png"
+		    rm "${folder}/base.png"
+		    cp "${folder}/${counter}.finished.png" "${folder}/base.png"
+		fi
 		iterator=$((iterator+1))
             done
-            ffmpeg -r 15 -i "${folder}/%03d.mpg.png" "${folder}/animation.avi"
-            rm "${folder}"/*.vrt
-	    rm "${folder}"/*.xml
-	    rm "${folder}"/*."${small_ext}"
-	    rm "${folder}"/*.mpg.png
+            ffmpeg -r 15 -i "${folder}/%03d.finished.png" "${folder}/animation.avi"
+            #rm "${folder}"/*.vrt
+	    #rm "${folder}"/*.xml
+	    #rm "${folder}"/*."${small_ext}"
+	    #rm "${folder}"/*.mpg.png
 	fi
     fi
 done
--- merge-date.sh
+++ merge-date.sh
@@ -6,23 +6,32 @@ set -x
 DIR=$1
 for subdir in $(find ${DIR} -maxdepth 1 -name '[0-9]*' -type d); do
     if [ -d "${subdir}" ]; then
-       # merge all the compressed files
-       subdir=$(basename ${subdir})
-       files=$(find "${DIR}/${subdir}" -name "*.compressed.tiff" -printf '%p ' | sort -u)
-       for file in $files
-       do
-	   filename="$(basename "${file}")"    # full filename without the path
-	   filebase="$(echo ${filename} | sed 's/.compressed.tiff//g')"  # filename without extension
-	   warped_file=${filebase}.warped.tiff
-           gdalwarp -of GTiff -srcnodata 0 -t_srs EPSG:3031 ${file} ${DIR}/${subdir}/${warped_file}
-       done
+        # merge all the compressed files
+        subdir=$(basename ${subdir})
+        files=$(find "${DIR}/${subdir}" -name "*.compressed.tiff" -printf '%p ' | sort -u)
+        for file in $files
+        do
+	    filename="$(basename "${file}")"    # full filename without the path
+	    filebase="$(echo ${filename} | sed 's/.compressed.tiff//g')"  # filename without extension
+	    warped_file=${filebase}.warped.tiff
 
-       gdal_merge.py -of GTiff -o ${DIR}/${subdir}.merged.tiff ${DIR}/${subdir}/*.warped.tiff
-       rm -rf ${DIR}/${subdir}
-       gdal_translate -of GTiff -scale 200 3000 -ot Byte ${DIR}/${subdir}.merged.tiff ${DIR}/${subdir}.scaled.tiff
-       rm ${DIR}/${subdir}.merged.tiff
-       #gdal_translate -of PNG ${DIR}/${subdir}.scaled.tiff ${DIR}/${subdir}.png
-       #gdalwarp -of GTiff -t_srs EPSG:3031 ${DIR}/${subdir}.scaled.tiff ${DIR}/${subdir}.warped.tiff
+
+            #gdalwarp -of GTiff -s_srs EPSG:4326 -novshiftgrid -srcnodata 0 -t_srs EPSG:3031 -r near -dstalpha -multi -of GTiff ${file} ${DIR}/${subdir}/${warped_file}
+            gdalwarp -of GTiff -tps -order 3 --refine_gcps 10 -s_srs EPSG:4326 -novshiftgrid -srcnodata 0 -t_srs '+proj=laea +lon_0=-68.5656738 +lat_0=-90 +datum=WGS84 +units=m +no_defs' -r near -dstalpha -multi -of GTiff ${file} ${DIR}/${subdir}/${warped_file}
+	    gdal_translate -of GTiff -scale 100 12813 0 255 -ot Byte ${DIR}/${subdir}/${warped_file} ${DIR}/${subdir}/${filebase}.translate.tiff
+	    nearblack -of VRT -nb 1 -near 0 -setalpha ${DIR}/${subdir}/${filebase}.translate.tiff
+        done
+
+
+        #gdal_merge.py -of GTiff -n 0 -a_nodata 0 -o ${DIR}/${subdir}.merged.tiff ${DIR}/${subdir}/*.translate.tiff
+        gdal_merge.py -ot Byte -of GTiff -n 0 -o ${DIR}/${subdir}.merged.tiff ${DIR}/${subdir}/*.translate.tiff
+        #set band 2 as alpha
+        gdal_translate ${DIR}/${subdir}.merged.tiff ${DIR}/${subdir}.merged.masked.tiff -b 1 -mask 2 -co COMPRESS=LZW --config GDAL_TIFF_INTERNAL_MASK YES
+        #rm -rf ${DIR}/${subdir}
+        #gdal_translate -of GTiff -scale 200 3000 -ot Byte ${DIR}/${subdir}.merged.tiff ${DIR}/${subdir}.scaled.tiff
+        #rm ${DIR}/${subdir}.merged.tiff
+        #gdal_translate -of PNG ${DIR}/${subdir}.scaled.tiff ${DIR}/${subdir}.png
+        #gdalwarp -of GTiff -t_srs EPSG:3031 ${DIR}/${subdir}.scaled.tiff ${DIR}/${subdir}.warped.tiff
     fi
 done
 
